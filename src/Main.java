675bd2e8e6a362824cf0b69b43392bc8717a144a
BratyshevTD <bratyshev2013@mail.ru>
2022-09-13 23:03:25 +0300

    Продолжение работы над алгоритмом. Задача 1, номер 3

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,12 @@ import java.util.Scanner;
 
 public class Main {
 
+    public static void printArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
     public static int[] randArray(int[] arr) {
         Random rand = new Random();
         for (int i = 0; i < arr.length; i++) {
@@ -14,32 +20,43 @@ public class Main {
 
     public static void solution(int[] arr, int x) {
         for (int i = 0; i < arr.length - 1; i++) {
+            int prev = 0, next = 0, k, cnt = 0;
             if (arr[i] == x)
-                for (int j = i; j < arr.length - 1; i++) {
-                    int prev = 0, next = 0, k = 0;
-                    if (j == 0)
+                for (int j = i; j < arr.length - 1; j++) {
+                    if (j == 0) {
                         prev = 0;
-                    else if (arr[j] != arr[j - 1])
+                        cnt++;
+                    } else if (arr[j] != arr[j - 1]) {
                         prev = arr[j - 1];
-                    if (j == arr.length - 1)
+                        cnt++;
+                    }
+                    if (j == arr.length - 1) {
                         next = 0;
-                    else if (arr[i] != arr[i + 1])
+                        cnt++;
+                    } else if (arr[i] != arr[i + 1]) {
                         next = arr[i + 1];
-                    k = (next + prev) / 2;
-                    arr[i] = k;
+                        cnt++;
+                    }
+                    if (cnt == 2) {
+                        k = (next + prev) / 2;
+                        arr[i] = k;
+                        cnt = 0;
+                    }
+
                 }
         }
+        printArray(arr);
     }
 
     public static void main(String[] args) {
-        System.out.println("Input n: ");
+//        System.out.println("Input n: ");
         System.out.print("Input x: ");
         Scanner scan = new Scanner(System.in);
-        int n = scan.nextInt();
+//        int n = scan.nextInt();
         int x = scan.nextInt();
-        int[] arr = new int[n];
-        int[] arr1 = {1, 2, 2, 5, 5};
+//        int[] arr = new int[n];
+        int[] arr1 = {3, 2, 2, 5, 7};
         solution(arr1, x);
-        randArray(arr);
+//        randArray(arr);
     }
 }
