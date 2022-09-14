0a03ab9437e06973a9d387536c7b8d3f7334bacc
bratyshevTD <bratyshev2013@mail.ru>
2022-09-15 01:10:09 +0300

    Доработал алгоритм, ввёл замерение времени, 1 таск номер 3

--- src/Main.java
+++ src/Main.java
@@ -1,62 +1,95 @@
 import java.util.Random;
 import java.util.Scanner;
-
 public class Main {
-
     public static void printArray(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
     }
-
-    public static int[] randArray(int[] arr) {
+    public static void randArray(int[] arr) {
         Random rand = new Random();
         for (int i = 0; i < arr.length; i++) {
-            arr[i] = rand.nextInt(2);
-            System.out.print(arr[i] + " ");
+            arr[i] = rand.nextInt(1);
+//            System.out.print(arr[i] + " ");
         }
-        return arr;
     }
-
-    public static void solution(int[] arr, int x) {
-        for (int i = 0; i < arr.length - 1; i++) {
-            int prev = 0, next = 0, k, cnt = 0;
-            if (arr[i] == x)
-                for (int j = i; j < arr.length - 1; j++) {
+    public static void solution(int[] arr, int x, int n) {
+        long start = System.currentTimeMillis();
+        int prev = 0, next = 0, k = 0;
+        boolean prevFlag = false, nextFlag = false;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == x) {
+                for (int j = i; j < arr.length; j++) {
                     if (j == 0) {
                         prev = 0;
-                        cnt++;
+                        prevFlag = true;
                     } else if (arr[j] != arr[j - 1]) {
                         prev = arr[j - 1];
-                        cnt++;
+                        prevFlag = true;
                     }
                     if (j == arr.length - 1) {
                         next = 0;
-                        cnt++;
+                        nextFlag = true;
                     } else if (arr[i] != arr[i + 1]) {
                         next = arr[i + 1];
-                        cnt++;
+                        nextFlag = true;
                     }
-                    if (cnt == 2) {
-                        k = (next + prev) / 2;
-                        arr[i] = k;
-                        cnt = 0;
+                    if (nextFlag && prevFlag) {
+                        k = (prev + next) / 2;
+                        break;
                     }
-
                 }
+                if (nextFlag && prevFlag) {
+                    arr[i] = k;
+                    prevFlag = false;
+                    nextFlag = false;
+                }
+            }
         }
-        printArray(arr);
+//        printArray(arr);
+        System.out.println();
+
+        long finish = System.currentTimeMillis();
+        long elapsed = finish - start;
+        System.out.println("_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-");
+        System.out.println("Количество входных данных: " + n);
+        System.out.println("Прошло времени, мс: " + elapsed);
+
     }
 
     public static void main(String[] args) {
-//        System.out.println("Input n: ");
-        System.out.print("Input x: ");
+        System.out.print("Input n: ");
         Scanner scan = new Scanner(System.in);
-//        int n = scan.nextInt();
-        int x = scan.nextInt();
-//        int[] arr = new int[n];
-        int[] arr1 = {3, 2, 2, 5, 7};
-        solution(arr1, x);
-//        randArray(arr);
+        int x = 0;
+        int n = 1000;
+        int[] arr1 = new int[n];
+        randArray(arr1);
+        solution(arr1, x, n);
+
+        n = 5000;
+        int[] arr2 = new int[n];
+        randArray(arr2);
+        solution(arr2, x, n);
+
+        n = 10000;
+        int[] arr3 = new int[n];
+        randArray(arr3);
+        solution(arr3, x, n);
+
+        n = 20000;
+        int[] arr4 = new int[n];
+        randArray(arr1);
+        solution(arr4, x, n);
+
+        n = 30000;
+        int[] arr5 = new int[n];
+        randArray(arr1);
+        solution(arr5, x, n);
+
+        n = 40000;
+        int[] arr6 = new int[n];
+        randArray(arr1);
+        solution(arr6, x, n);
+
     }
 }
